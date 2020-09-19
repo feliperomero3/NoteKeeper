84c59a2f533365c9191f606f82423d1596a029ad
Felipe Romero <feliperomero3@outlook.com>
2020-09-18 22:27:58 -0500

    Implement Activity Interaction and Startup Behavior

--- app/src/main/java/mx/feliperomero/notekeeper/NoteListActivity.java
+++ app/src/main/java/mx/feliperomero/notekeeper/NoteListActivity.java
@@ -3,8 +3,10 @@ package mx.feliperomero.notekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,7 +16,6 @@ import com.google.android.material.snackbar.Snackbar;
 import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
-    private ArrayAdapter<NoteInfo> mAdapterNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +40,18 @@ public class NoteListActivity extends AppCompatActivity {
         final ListView listNotes = findViewById(R.id.list_notes);
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
-        mAdapterNotes = new ArrayAdapter<>(this,
+        ArrayAdapter<NoteInfo> mAdapterNotes = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, notes);
 
         listNotes.setAdapter(mAdapterNotes);
+
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
